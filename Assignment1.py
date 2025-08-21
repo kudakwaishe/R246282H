b7b7f1e2bac015d4d33411a2d67dfb6bf039ef5e
kudakwaishe <kayjay.makombore@gmail.com>
2025-08-21 11:00:39 +0200

    Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -9,8 +9,9 @@ balance = TOTAL_FEES - paid
 if balance == 0:
     print("Lecture access granted!")
 elif balance > 0:
-    print("Please settle your balance of " + str(balance) + " inorder to receive lecture access.")
+    print("Please settle your balance of " + str(balance) + " in order to receive lecture access.")
 elif balance < 0:
     print("Your change will be credited. (maybe.)")
 else:
+
     print("Invalid input.")
